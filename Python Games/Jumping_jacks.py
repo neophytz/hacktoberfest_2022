af1915696a5b53745a37cd2f2afc11da44439b59
Gourav Singh <49956754+GouravSingh2580@users.noreply.github.com>
2022-10-04 16:55:37 +0530

    Create Jumping_jacks.py

---
+++ Python Games/Jumping_jacks.py
@@ -0,0 +1,53 @@
+import replit
+import time
+replit.clear()
+
+print("Watch and enjoy a man doing jumping jacks.")
+print ("One jumping jack is equal to one second.")
+print ("How long can you stay on?")
+time.sleep(4)
+a = 0
+print(a)
+
+while True:
+  for i in range(1):
+    print (" o ")
+    print ("/|\ ")
+    print ("/ \ ")
+    time.sleep(.2)
+    replit.clear()
+    print(a, "seconds")
+   
+  for i in range(1): 
+    print ("\o/ ")
+    print (" | ")
+    print ("| | ")
+    a = a + 1
+    time.sleep(.2)
+    replit.clear()
+    print(a, "seconds")
+    
+  for i in range(1):
+    print (" o ")
+    print ("/|\ ")
+    print ("/ \ ")
+    time.sleep(.2)
+    replit.clear()
+    print(a, "seconds")
+   
+  for i in range(1): 
+    print ("\o/ ")
+    print (" | ")
+    print ("| | ")
+    time.sleep(.2)
+    replit.clear()
+    print(a, "seconds")
+
+    
+  for i in range(1):
+    print (" o ")
+    print ("/|\ ")
+    print ("/ \ ")
+    time.sleep(.2)
+    replit.clear()
+    print(a, "seconds")
